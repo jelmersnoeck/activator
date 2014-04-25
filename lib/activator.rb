fa83a1489781f4c49d48f26e18c360ccb4a42ac3
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2014-04-24 21:47:02 -0500

    Activator: add basic activator implementation.
    Add basic implementation of the activator system.

--- lib/activator.rb
+++ lib/activator.rb
@@ -1,2 +1,25 @@
 module Activator
+  extend ActiveSupport::Concern
+
+  included do
+    after_save :deactivate_others
+  end
+
+  module ClassMethods
+    def active
+      find_by(active: true)
+    end
+  end
+
+  def deactivate
+    self.update_attributes(active: false)
+  end
+
+  def deactivate_others
+    return unless self.active?
+    self.class.where('id <> :id', id: self.id).where(active: true).
+      map(&:deactivate)
+  end
 end
+
+ActiveRecord::Base.send :include, Activator
--- test/activator_test.rb
+++ test/activator_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ActivatorTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Activator
+  test 'it can find the active items' do
+    Project.create(active: false)
+    project = Project.create(active: true)
+
+    assert_equal project, Project.active
+  end
+
+  test 'it deactivates old items when creating new active ones' do
+    project1 = Project.create(active: true)
+    assert project1.active?
+
+    project2 = Project.create(active: true)
+    assert project2.active?
+    refute project1.reload.active?
+  end
+
+  test 'it deactivates other items when setting another item to active' do
+    project1 = Project.create(active: false)
+    project2 = Project.create(active: true)
+
+    refute project1.active?
+    assert project2.active?
+
+    project1.update_attributes(active: true)
+
+    assert project1.reload.active?
+    refute project2.reload.active?
   end
 end
