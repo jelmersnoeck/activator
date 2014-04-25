d040f45e12691fd16a0ea5190b9bc2d532d84cbc
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2014-04-25 09:20:34 -0500

    Activator: try active? method.

--- lib/activator.rb
+++ lib/activator.rb
@@ -18,7 +18,7 @@ module Activator
   end
 
   def deactivate_others
-    return unless self.active?
+    return unless self.try(:active?)
     self.class.where('id <> :id', id: self.id).where(active: true).
       map(&:deactivate)
   end
