0fee8556fd0f62d34a33c65c475aed4b537e5631
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2014-04-24 21:53:43 -0500

    TestDummy: add User schema.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424234705) do
+ActiveRecord::Schema.define(version: 20140425024510) do
 
   create_table "projects", force: true do |t|
     t.boolean  "active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
